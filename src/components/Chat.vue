d3c38e1e10aacf807db7320e7238e12a13d8a7a7
Mir Imad Ahmed <mirimadahmed@outlook.com>
2019-02-15 18:53:56 +0500

    basic chat page with styling

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
+        <div class="messages-wrapper">
 
+        </div>
+        <p>Your message goes here</p>
+        <div class="input-wrapper">
+            <input type="text" class="message-text">
+            <button class="new-message">Send Message</button>
+        </div>
     </div>
 </template>
 
@@ -11,5 +18,24 @@
 </script>
 
 <style scoped>
-
+.messages-wrapper{
+    width: 800px;
+    height: 500px;
+    background: pink;
+    margin:auto;
+}
+.input-wrapper {
+    width: 800px;
+    height: 40px;
+    margin: auto;
+}
+.message-text {
+    width: 80%;
+    height: inherit;
+}
+.new-message {
+    width: 15%;
+    height: inherit;
+    margin-left: 20px;
+}
 </style>
